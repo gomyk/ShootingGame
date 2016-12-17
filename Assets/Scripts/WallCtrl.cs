c9bcd9dea7e5ea9c45554b6c48fb3eac2f4ed912
Moonwon Kim <gomyk@naver.com>
2016-12-18 03:42:28 +0900

    Unity

---
+++ Assets/Scripts/WallCtrl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCtrl : MonoBehaviour {
+    public GameObject sparkEffect;
+	// Use this for initialization
+	void OnCollisionEnter(Collision coll)
+    {
+        if (coll.collider.tag == "Bullet")
+        {
+            GameObject spark = (GameObject)Instantiate(sparkEffect, coll.transform.position, Quaternion.identity);
+            Destroy(spark, spark.GetComponent<ParticleSystem>().duration + 0.2f);
+            Destroy(coll.gameObject);
+            
+        }
+    }
+}
---
+++ Assets/UIMgr.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIMgr : MonoBehaviour {
+
+	// Use this for initialization
+	
+     public void  OnClickStartBtn()
+    {
+        Debug.Log("ClickButton");
+    }
+}
